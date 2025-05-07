390a01d340d5f569ad96256e6232820d541bba3b
yi-takahashi-kureha <takahashi_kureha@y-i-group.co.jp>
2025-05-07 16:35:07 +0900

    fix: foreachをネスト構造に変更

--- q16/answer.php
+++ q16/answer.php
@@ -9,57 +9,36 @@
 <body>
 <h1>書籍一覧</h1>
 
-<table>
-    <thead>
-        <tr>
-            <th>書籍名</th>
-            <th>価格 (円)</th>
-            <th>著者名</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php
+<?php
+    $books = [
+        ['title' => 'PHP入門', 'price' => 2400, 'author' => '山田 太郎'],
+        ['title' => 'JavaScript実践ガイド', 'price' => 3200, 'author' => '佐藤 花子'],
+        ['title' => 'データベース設計の基礎', 'price' => 2800, 'author' => '田中 一郎'],
+    ];
 
-        $books = [
-            [
-                'title' => 'PHP入門',
-                'price' => 2400,
-                'author' => '山田 太郎'
-            ],
-            [
-                'title' => 'JavaScript実践ガイド',
-                'price' => 3200,
-                'author' => '佐藤 花子'
-            ],
-            [
-                'title' => 'データベース設計の基礎',
-                'price' => 2800,
-                'author' => '田中 一郎'
-            ],
-            [
-                'title' => 'Webデザインの教科書',
-                'price' => 2600,
-                'author' => '鈴木 次郎'
-            ]
-        ];
+    foreach ($books as $book) {
+        echo "<div>";
+        $outputParts = [];
+        foreach ($book as $key => $value) {
+            $displayKey = '';
+            $displayValue = htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
 
-        foreach ($books as $book) {
-            echo "<tr>\n";
+            if ($key === 'title') {
+                $displayKey = '書籍名';
+            } elseif ($key === 'price') {
+                $displayKey = '価格';
+                $displayValue = number_format((int)$value) . '円';
+            } elseif ($key === 'author') {
+                $displayKey = '著者';
+            }
 
-            $safe_title = htmlspecialchars($book['title'], ENT_QUOTES, 'UTF-8');
-            echo "    <td>" . $safe_title . "</td>\n";
-
-            $formatted_price = number_format($book['price']);
-            $safe_price = htmlspecialchars($formatted_price, ENT_QUOTES, 'UTF-8');
-            echo "    <td>" . $safe_price . "</td>\n";
-
-            $safe_author = htmlspecialchars($book['author'], ENT_QUOTES, 'UTF-8');
-            echo "    <td>" . $safe_author . "</td>\n";
-
-            echo "</tr>\n";
+            if ($displayKey) {
+                $outputParts[] = "<span>" . htmlspecialchars($displayKey, ENT_QUOTES, 'UTF-8') . "：</span><span class='value'>" . $displayValue . "</span>";
+            }
         }
-        ?>
-    </tbody>
-</table>
+        echo implode("／", $outputParts);
+        echo "</div>\n";
+    }
+    ?>
 </body>
 </html>
